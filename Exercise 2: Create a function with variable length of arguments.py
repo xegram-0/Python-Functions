da4f0dfc95f439408ba28aac9108eb04133aee1e
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-04-23 15:16:44 +0700

    added problem description

--- Exercise 2: Create a function with variable length of arguments.py
+++ Exercise 2: Create a function with variable length of arguments.py
@@ -1,6 +1,25 @@
 # Exercise 2: Create a function with 
 # variable length of arguments
 
+# Function call:
+
+# # call function with 3 arguments
+# func1(20, 40, 60)
+
+# # call function with 2 arguments
+# func1(80, 100)
+
+# Expected Output:
+
+# Printing values
+# 20
+# 40
+# 60
+
+
+# Printing values
+# 80
+# 100
 
 def func1(*num):
     print("Printing values:")
