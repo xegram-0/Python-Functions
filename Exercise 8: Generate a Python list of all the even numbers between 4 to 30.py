2025dc06e1b31e6984ef6e00b6f2527a9f0514dc
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-04-23 16:35:30 +0700

    Solved exercise 8.

---
+++ Exercise 8: Generate a Python list of all the even numbers between 4 to 30.py
@@ -0,0 +1,19 @@
+# Exercise 8: Generate a Python list of 
+# all the even numbers between 4 to 30
+
+# Expected Output:
+
+# [4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28]
+def generateList(start:int, end:int):
+    evenList:list = []
+    for i in range(start,end+1):
+        if i%2 == 0:
+            evenList.append(i)
+    print(evenList)
+def main():
+    if __name__=="__main__":
+        start = int(input("Enter starting number of the list: "))
+        end = int(input("Enter ending number of the list: "))
+        generateList(start,end)
+
+main()
